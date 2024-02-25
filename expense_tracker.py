1a842ffa1525ebeee940ff6f7f5eb5ec96ab82b3
jason5793 <jasonvlog81@gmail.com>
2024-02-25 17:38:31 +0530

    here adding to check thhe valid code is in range or not

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,10 +10,30 @@ def main():
     summarize()
 
 def get_user_expense():
-    print("getting user expense:")
+    print("Getting user expense:")
     expense_name=input("Enter expense name:")
-    expense_amount=float(input("Enter expense name:"))
+    expense_amount=float(input("Enter expense amount:"))
     print(f"You have entered:{expense_name},{expense_amount}")
+
+    expense_categories=[
+        "Food",
+        "Home",
+        "Work",
+        "Fun",
+        "Misc",
+    ]
+    while True:
+        print("Select a Category: ")
+        for i,category_name in enumerate(expense_categories):
+            print(f"{i+1}. {category_name}")
+        value_range =f"[1-{len(expense_categories)}]"
+        select_index=int(input(f"Enter a category number{value_range}:"))-1
+        
+        if select_index in range(len(expense_categories)):
+            break
+        else:
+            print("Invalid Category. Please try again")
+
 def save_expense_to_file():
     print(f"Save user expense:")
 
